f850f9768e490c7a83f648ca14ec1579628e3b0f
Vyom Kulshrestha <hardikshobhit87@gmail.com>
2024-11-28 14:20:28 +0530

    Delete flask backend directory

--- flask backend/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from app.controllers.auth_controller import auth_blueprint
-from app.controllers.ml_controller import ml_blueprint
-
-def setup_routes(app):
-    app.register_blueprint(auth_blueprint, url_prefix="/auth")
-    app.register_blueprint(ml_blueprint, url_prefix="/ml")
--- flask backend/conig.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "supersecretjwtkey")
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
